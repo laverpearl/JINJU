3665501a952159c48e09213337e4dec6ba2681f1
laverpearl <k2811jj@gmail.com>
2021-04-12 01:51:03 +0900

    백준문제 C++ 첫번째

---
+++ baekjoon_practice/2588.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // a=472 b=385 c=3 d=8 e=5
+    // vscode에서 c++하려면 단축키 ----> command shift R
+    int a;
+    int b;
+    cin>>a>>b;
+    int c = b/100;
+    int d = (b/10)%30;
+    int e = b%380;
+
+    cout<<a*e<<'\n';
+    cout<<a*d<<'\n';
+    cout<<a*c<<'\n';
+    cout<<a*b;
+    
+}
