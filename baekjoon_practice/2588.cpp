4f4f1c3f8734769a74710ac4f475f87d4f8d891b
laverpearl <k2811jj@gmail.com>
2021-04-26 02:34:06 +0900

    2588정답

--- baekjoon_practice/2588.cpp
+++ baekjoon_practice/2588.cpp
@@ -3,21 +3,13 @@ using namespace std;
 
 int main()
 {
-    // a=472 b=385 c=3 d=8 e=5
-    // vscode에서 c++하려면 단축키 ----> command shift R
-    int a;
-    int b;
-    cin>>a>>b;
-    int c = b/100;
-    int d = (b/10)%30;
-    int e = b%380;
+    int a,b;
 
-    cout<<a*e<<'\n';
-    cout<<a*d<<'\n';
-    cout<<a*c<<'\n';
-    cout<<a*e+a*d*10+a*c*100;
+    cin >> a >> b;
 
-
-    // 틀렸음 문제 다시풀기
+    cout<<a*(b%10)<<'\n';
+    cout<<a*((b%100)/10)<<'\n';
+    cout<<a*(b/100)<<'\n';
+    cout<<a*b;
 
 }
