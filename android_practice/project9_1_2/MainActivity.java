68ac70bcd02d30317e91d2e31b34b0c5abf5f2d7
laverpearl <k2811jj@gmail.com>
2021-05-13 02:40:20 +0900

    오류 수정

--- android_practice/project9_1_2/MainActivity.java
+++ android_practice/project9_1_2/MainActivity.java
@@ -16,13 +16,27 @@ import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
+
     ImageButton ibZoomin, ibZoomout, ibRotate, ibBright, ibDark, ibGray;
     MyGraphicView graphicView;
-
     static float scaleX = 1, scaleY =1;
     static float angle = 0;
     static float color = 1;
     static float satur = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("201639298 김진주");
+
+        LinearLayout pictureLayout = (LinearLayout) findViewById(R.id.pictureLayout);
+        graphicView = (MyGraphicView) new MyGraphicView(this);
+        pictureLayout.addView(graphicView);
+
+        clickIcons();
+
+    }
 
     private void clickIcons(){
         ibZoomin = (ImageButton) findViewById(R.id.ibZoomin);
@@ -33,7 +47,14 @@ public class MainActivity extends AppCompatActivity {
                 graphicView.invalidate();
             }
         });
-
+        ibZoomout = (ImageButton) findViewById(R.id.ibZoomout);
+        ibZoomout.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                scaleX = scaleX - 0.2f;
+                scaleY = scaleY - 0.2f;
+                graphicView.invalidate();
+            }
+        });
         ibRotate = (ImageButton) findViewById(R.id.ibRotate);
         ibRotate.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,17 +63,22 @@ public class MainActivity extends AppCompatActivity {
                 graphicView.invalidate();
             }
         });
-
         ibBright = (ImageButton) findViewById(R.id.ibBright);
         ibBright.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 color = color + 0.2f;
                 graphicView.invalidate();
-
             }
         });
-
+        ibDark = (ImageButton) findViewById(R.id.ibDark);
+        ibDark.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                color = color - 0.2f;
+                graphicView.invalidate();
+            }
+        });
         ibGray = (ImageButton) findViewById(R.id.ibGray);
         ibGray.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -62,63 +88,45 @@ public class MainActivity extends AppCompatActivity {
                 graphicView.invalidate();
             }
         });
-
-    }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        setTitle("201639298 김진주");
-        clickIcons();
-
-        LinearLayout pictureLayout = (LinearLayout) findViewById(R.id.pictureLayout);
-        graphicView = (MyGraphicView) new MyGraphicView(this);
-        pictureLayout.addView(graphicView);
-
     }
 
     private static class MyGraphicView extends View {
         public MyGraphicView(Context context) {
             super(context);
         }
+
+        @Override
+        protected void onDraw(Canvas canvas) {
+            super.onDraw(canvas);
+            int cenX = this.getWidth()/2;
+            int cenY = this.getHeight()/2;
+            canvas.scale(scaleX, scaleY, cenX, cenY);
+            canvas.rotate(angle, cenX, cenY);
+
+            Paint paint = new Paint();
+            float[] array = { color, 0, 0, 0, 0,
+                    0, color, 0, 0, 0,
+                    0, 0, color, 0, 0,
+                    0, 0, 0, 1, 0, };
+
+            ColorMatrix cm = new ColorMatrix(array);
+
+            if (satur == 0)
+                cm.setSaturation(satur);
+            paint.setColorFilter(new ColorMatrixColorFilter(cm));
+
+            Bitmap picture = BitmapFactory.decodeResource(getResources(),
+                    R.drawable.jelly);
+            int picX = (this.getWidth()-picture.getHeight())/2;
+            int picY = (this.getHeight()-picture.getHeight())/2;
+
+            canvas.drawBitmap(picture, picX, picY, paint);
+
+            picture.recycle();
+
+        }
     }
-
-
-
-
-
-    //onDraw에서 오류,,
-    protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
-
-        canvas.rotate(angle, cenX, cenY);
-        graphicView.invalidate();
-        //canvas.scale();
-
-        Bitmap picture = BitmapFactory.decodeResource(getResources(),
-                R.drawable.jelly);
-
-        int picX = (this.getWidth() - picture.getWidth()) /2;
-        int picY = (this.getWidth() - picture.getWidth()) /2;
-        int cenX = this.getWidth() /2;
-        int cenY = this.getWidth() /2;
-
-        Paint paint = new Paint();
-        float[] array = { 2, 0, 0, 0, -25,
-                          0, 2, 0, 0, -25,
-                          0, 0, 2, 0, -25,
-                          0, 0, 0, 1, 0, };
-
-        if (satur == 0) cm.setSaturation(satur);
-
-        ColorMatrix cm = new ColorMatrix(array);
-        paint.setColorFilter(new ColorMatrixColorFilter(cm));
-
-        canvas.drawBitmap(picture, picX, picY, paint);
-        canvas.scale(scaleX, scaleY, cenX, cenY);
-
-        picture.recycle();
-    }
-
 }
+
+// android:icon="@drawable/jelly"
+// android:hardwareAccelerated="false"
